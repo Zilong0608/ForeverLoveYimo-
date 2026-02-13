544c9e5064799da07bedd28dcdd697781f2e2dfd
Zilong0608 <excalibur8680608@gmail.com>
2026-02-13 18:02:51 +1100

    update base for pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: "/214/",
+    base: "/forever-love-yimo/",
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
